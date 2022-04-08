a055bde74e592dc09b8de45e4284cb7c956f447e
hong <hcy1997@naver.com>
2022-04-08 22:24:40 +0900

    update - quest_delete

--- game/urls.py
+++ game/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('competition/', views.competition, name='competition'),
     path('quest/', views.quest, name='quest'),
+    path('quest/delete', views.quest_delete, name='quest_delete'),
 ]
