4716c9e49f288cc6ff9e1f05fe9a901c92d48e3f
hong <hcy1997@naver.com>
2022-04-13 10:37:37 +0900

    update - info/view get try except

--- info/views.py
+++ info/views.py
@@ -42,18 +42,20 @@ def content_type(request, type):
 
 def content_detail(request, pk):
     if request.method == 'GET':
-        content = Content.objects.get(id=pk)
-        type = content.type
-        if type == 'food':
-            data = Food.objects.get(content=content)
-        elif type == 'diet_plan':
-            data = DietPlan.objects.get(content=content)
-        elif type == 'workout':
-            data = Workout.objects.get(content=content)
-        elif type == 'workout_routine':
-            data = WorkoutRoutine.objects.get(content=content)
-        return render(request, 'info/content_detail.html', {'type': type, 'data': data})
-
+        try:
+            content = Content.objects.get(id=pk)
+            type = content.type
+            if type == 'food':
+                data = Food.objects.get(content=content)
+            elif type == 'diet_plan':
+                data = DietPlan.objects.get(content=content)
+            elif type == 'workout':
+                data = Workout.objects.get(content=content)
+            elif type == 'workout_routine':
+                data = WorkoutRoutine.objects.get(content=content)
+            return render(request, 'info/content_detail.html', {'type': type, 'data': data})
+        except:
+            return redirect(request.headers['Referer'])
 
 @login_required()
 def content_save(request, pk):
